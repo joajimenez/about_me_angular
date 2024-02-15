7741bde383a187b3c3cc5a3f9c81b0eb4f3a8e6e
Joan Jimenez <jimenez.donaldson@gmail.com>
2024-02-15 07:56:36 -0400

    Add Bootstrap and Bootstrap Icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // template: `<div class="container">
+  //   <h1>Bienvenido a la pagina de Joan Jimenez</h1>
+  //   <h4>Haz click en el botón 👀</h4>
+  //   <button>Entrar</button>
+  // </div> `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'about_me';
