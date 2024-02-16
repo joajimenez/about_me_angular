bf571ca67022ab8003eba889b4dfeb8365b1d15d
Joan Jimenez <jimenez.donaldson@gmail.com>
2024-02-16 15:44:42 -0400

    Initial push

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgOptimizedImage],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  { path: '/', component: AppComponent },
-  { path: 'about', component: AboutComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', title:'Home Page', component: HomeComponent },
+  { path: 'about', title:'Acerca de', component: AboutComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
